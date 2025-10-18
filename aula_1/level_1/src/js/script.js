ab1e55486b1afce13ea1e8e91147bf736d952d32
williampss <williampatriciodosantos28@gmail.com>
2025-10-18 16:38:11 -0300

    feat

---
+++ aula_1/level_1/src/js/script.js
@@ -0,0 +1,30 @@
+// tipo nome = valor
+
+// TIPOS DE VARIAVEIS
+
+// var
+// let : variavelmudtavel
+// const: variavel fixa, não muda
+
+
+// VAR PODE COMEÇAR COM O VALOR E DEPOIS RECEBER UMA ALTERAÇÃO
+
+var nome = "William"
+ nome = "Wallas"
+
+ console.log(nome)
+
+
+// LET Declara variáveis com escopo de bloco: Uma variável declarada 
+// com let só é acessível dentro do bloco {} em que foi definida. 
+
+let lampada = "ligada"
+// let lampada = desligado NÃO PERMITE A REDECLARAÇÃO DA VARIAVEL
+console.log(`a lampada está ${lampada}`)
+
+
+// CONST MAIS USADA QUANDO É PARA ALGO QUE NÃO É MUTAVEL
+
+const pi = 3.14
+
+console.log(pi)
---
+++ aula_1/level_1/src/js/tiposDeDados.js
@@ -0,0 +1,69 @@
+// //--------------STRING/NUMBER-----------------
+// // let nome = "Raimundo"
+//  var idade = 17
+// // const cpf = 434344335677
+
+// // console.log(`Bem-vindo ${nome}\nVocê tem ${idade} anos de idade\nE é o usuario do CPF ${cpf}`)
+
+// //--------------BOOLEAN----------------------
+// let maiorDeIdade = true
+// let menorDeIdade = false
+
+// //--------------NULL------------------------
+// let desconto = null
+
+// //--------------UNDEFINED-----------------
+// let indefinido
+
+// //--------------ARRAY-----------------
+// let  frutas = ["maca","toranja","abacaxi"]
+
+// // ADICIONA UM ITEM EM PRIMEIRO
+// frutas.unshift("melancia")
+
+// // ADICIONA UM ITEM EM ULTIMO
+// frutas.push("pera")
+
+
+// //APAGA O PRIMEIRO ITEM
+// frutas.shift()
+
+// //APAGA O ULTIMO
+// frutas.pop()
+
+// // console.log(frutas)
+// console.table(frutas)
+
+// let idades = [10,20,30,40,50]
+// console.log(idade[3])
+
+// let mercado = ["arroz",20,"melancia",true]
+
+
+// let favoriteColors = ["vermelho","azul escuro","preto"]
+
+// console.log(`QUais são suas cores favoritas?\nMinhas cores favoritas são ${favoriteColors}`)
+
+
+// let numbers = [2,3,5];
+
+// console.log(`${numbers[0]} + ${numbers[1]} = ${numbers[0] + numbers[1]}`)
+
+// //--------------OBJETOS-----------------..
+
+// let aluno = {
+//     nome: "William",
+//     idade:20,
+//     id: 4000000000589
+// }
+
+// console.log(`NOME:${aluno.nome}\n IDADE:${aluno.idade}\n ID:${aluno.id}`)
+
+
+let livro = {
+    titulo: "Teoria da relatividade",
+    autor: "Albert Einstein",
+    paginas: 508
+}
+
+console.log(`titulo:${livro.titulo}\n autor:${livro.autor}\n páginas:${livro.paginas}`)
